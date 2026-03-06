92dfb43a6acfbaa8b346423d48a53c57abf31b47
sndikawa1-web <sndikawa1@gmail.com>
2026-03-06 05:01:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,3 @@ GROUP_ID = int(os.environ.get('GROUP_ID', 0))
 
 # Irak Saati
 IRAQ_TZ = timezone(timedelta(hours=3))
-
-# Seviye Sistemi
-LEVEL_FORMULA = {
-    'easy': {'max_level': 15, 'xp_per_level': 200},
-    'medium': {'max_level': 30, 'xp_per_level': 300},
-    'hard': {'xp_per_level': 500}
-}
